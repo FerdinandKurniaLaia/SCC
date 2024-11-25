926b8c1645b81e6745e623c96f3ba1d00239432c
FerdinandKurniaLaia <chandra23ti@mahasiswa.pcr.ac.id>
2024-11-25 11:10:19 +0700

    oi ferdinand

---
+++ app/Http/Controllers/KelolaPelamarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KelolaPelamar;
+use App\Http\Requests\StoreKelolaPelamarRequest;
+use App\Http\Requests\UpdateKelolaPelamarRequest;
+
+class KelolaPelamarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKelolaPelamarRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KelolaPelamar $kelolaPelamar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KelolaPelamar $kelolaPelamar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKelolaPelamarRequest $request, KelolaPelamar $kelolaPelamar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KelolaPelamar $kelolaPelamar)
+    {
+        //
+    }
+}
--- app/Policies/KelolaPelamarPolicy.php
+++ app/Policies/KelolaPelamarPolicy.php
@@ -2,7 +2,7 @@
 
 namespace App\Policies;
 
-use App\Models\Kelola_Pelamar;
+use App\Models\KelolaPelamar;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -19,7 +19,7 @@ class KelolaPelamarPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Kelola_Pelamar $kelolaPelamar): bool
+    public function view(User $user, KelolaPelamar $kelolaPelamar): bool
     {
         //
     }
@@ -35,7 +35,7 @@ class KelolaPelamarPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Kelola_Pelamar $kelolaPelamar): bool
+    public function update(User $user, KelolaPelamar $kelolaPelamar): bool
     {
         //
     }
@@ -43,7 +43,7 @@ class KelolaPelamarPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Kelola_Pelamar $kelolaPelamar): bool
+    public function delete(User $user, KelolaPelamar $kelolaPelamar): bool
     {
         //
     }
@@ -51,7 +51,7 @@ class KelolaPelamarPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Kelola_Pelamar $kelolaPelamar): bool
+    public function restore(User $user, KelolaPelamar $kelolaPelamar): bool
     {
         //
     }
@@ -59,7 +59,7 @@ class KelolaPelamarPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Kelola_Pelamar $kelolaPelamar): bool
+    public function forceDelete(User $user, KelolaPelamar $kelolaPelamar): bool
     {
         //
     }
